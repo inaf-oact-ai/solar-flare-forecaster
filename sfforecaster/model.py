4f85e7d68fe9020eb040a173250400c7f21aa14e
Simone Riggi <simone.riggi@gmail.com>
2025-08-27 14:32:18 +0200

    Fix missing import

--- sfforecaster/model.py
+++ sfforecaster/model.py
@@ -14,6 +14,7 @@ import numpy as np
 # - TORCH
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from transformers import VideoMAEForVideoClassification
 from transformers import ViTForImageClassification
 
