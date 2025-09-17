257a9c54dd76cd157c7f79a208f8c8965af66872
Simone Riggi <simone.riggi@gmail.com>
2025-09-17 18:27:10 +0200

    Trying to make moirai work

--- sfforecaster/model.py
+++ sfforecaster/model.py
@@ -189,6 +189,8 @@ class MoiraiForSequenceClassification(torch.nn.Module):
 		
 		d_model = getattr(self.backbone, "d_model", 384)
 		self.config = MoiraiTSConfig(num_labels=num_labels, d_model=d_model)
+		print("self.model.num_labels")
+		print(self.model.num_labels)
 
 		#if freeze_backbone:
 		#	for p in self.backbone.parameters():
